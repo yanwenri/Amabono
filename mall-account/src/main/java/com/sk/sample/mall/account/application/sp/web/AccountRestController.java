322c65218d68ed64a181aa6e18b9d7abebb0dbee
yanwenri <50934392+yanwenri@users.noreply.github.com>
2019-05-23 15:34:27 +0900

    thymeleaf html add

--- mall-account/src/main/java/com/sk/sample/mall/account/application/sp/web/AccountRestController.java
+++ mall-account/src/main/java/com/sk/sample/mall/account/application/sp/web/AccountRestController.java
@@ -1,5 +1,22 @@
 package com.sk.sample.mall.account.application.sp.web;
 
-public class AccountRestController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+@RequestMapping("/account/*")
+public class AccountRestController {
+//	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping("login")
+	public String login() {
+		return "login";
+	}
+	
+//	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping("account")
+	public String account() {
+		return "account";
+	}
 }
